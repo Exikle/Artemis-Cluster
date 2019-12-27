b2f66172811fcdf7f261e79241063bbc391d812d
Exikle <dixondcunha@gmail.com>
2019-12-27 10:53:35 +0000

    moved drive around in cluster

--- rook/alias-toolbox.sh
+++ rook/alias-toolbox.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
+
 alias ceph="kubectl -n rook-ceph exec -it $(kubectl -n rook-ceph get pod -l 'app=toolbox' -o jsonpath='{.items[0].metadata.name}') ceph"
--- rook/prep-script/drive_prep_pkn002l.sh
+++ rook/prep-script/drive_prep_pkn002l.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sudo dmsetup remove_all && echo  "Removed Ceph dmsetup"
+sudo dmsetup remove_all && echo  "Removed ceph label from all drives"
 sudo rm -r /ceph && echo "Deleted /ceph"
-sudo rm -r /var/lib/rook && echo "Delete /var/lib/rook"
+sudo rm -r /var/lib/rook && echo "Deleted /var/lib/rook"
 sudo wipefs -a /dev/sdb && echo "Device: /dev/sdb wiped"
 sudo wipefs -a /dev/sdc && echo "Device: /dev/sdc wiped"
 sudo wipefs -a /dev/sdd && echo "Device: /dev/sdd wiped"
