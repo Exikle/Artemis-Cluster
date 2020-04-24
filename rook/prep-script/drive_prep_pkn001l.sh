6dd1dd75e8ceb220e637778717b3ed3d0f9b846d
Exikle <dixondcunha@gmail.com>
2020-04-23 22:05:55 -0400

    Updated to rook v1.3.2

--- rook/prep-script/drive_prep_pkn001l.sh
+++ rook/prep-script/drive_prep_pkn001l.sh
@@ -3,3 +3,6 @@
 sudo dmsetup remove_all && echo  "Removed ceph label from all drives"
 sudo rm -r /ceph && echo "Deleted /ceph"
 sudo rm -r /var/lib/rook && echo "Deleted /var/lib/rook"
+sudo wipefs -a /dev/sdc && echo "Device: /dev/sdc wiped"
+sudo wipefs -a /dev/sdd && echo "Device: /dev/sdd wiped"
+sudo wipefs -a /dev/sde && echo "Device: /dev/sde wiped"
