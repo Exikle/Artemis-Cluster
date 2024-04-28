013625fc0b1121ca4260d83b00c4e7afb094f708
Dixon D'Cunha <dixondcunha@gmail.com>
2024-04-28 03:14:44 -0400

    replaced Rook Ceph with democratic-csi

--- rook/alias-toolbox.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-#Can only be run after toolbox pod is running, which needs the cluster up
-
-alias ceph="kubectl exec -n rook-ceph $(kubectl -n rook-ceph get pod -l "app=rook-ceph-tools" -o jsonpath='{.items[0].metadata.name}') -- ceph"
