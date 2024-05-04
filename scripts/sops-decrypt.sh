95bf24f2e68156a4b17544588ebdd57453430386
Dixon D'Cunha <dixondcunha@gmail.com>
2024-05-04 18:10:08 -0400

    mvoed remaining secrets into cluster, not enrcypted but will change

--- scripts/sops-decrypt.sh
+++ scripts/sops-decrypt.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-read -p "Please Enter File to Decrypt: " $file
-sops --decrypt --age $(cat $SOPS_AGE_KEY_FILE |grep -oP "public key: \K(.*)") --encrypted-regex '^(data|stringData)$' --in-place $file
+read -p "Please Enter File to Encrypt: " $file
+sops --encrypt --age $(cat $SOPS_AGE_KEY_FILE |grep -oP "public key: \K(.*)") --encrypted-regex '^(data|stringData)$' --in-place $file
